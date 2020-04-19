9a45108543297822c1ff60cb71bf88f383d27cc8
soompy <sooper1137@gmail.com>
2020-04-19 23:48:29 +0900

    바나프레소 웹 vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="nav">
+      <h1><img src="./assets/logo.png"></h1>
+      <div class="link">
+         <router-link to="/">Home</router-link> |
+         <router-link to="/Menu">Menu</router-link>
+         <router-link to="/Store">Store</router-link>
+      </div>
+      
+    </div>
+    
     <router-view/>
+
+    <footer id="footer">
+      <ul class="row">
+        <li class="col-sm-3"><a href="#">홈</a></li>
+        <li class="col-sm-3"><a href="#">카테고리</a></li>
+        <li class="col-sm-3"><a href="#">검색</a></li>
+        <li class="col-sm-3"><a href="#" class="last">마이페이지</a></li>
+      </ul>
+    </footer>
+
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/views/Home'
+import Menu from '@/views/Menu'
+import Store from '@/views/Store'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Menu',
+      name: 'Menu',
+      component: Menu
+    },
+    {
+      path: '/Store',
+      name: 'Store',
+      component: Store
     }
   ]
 })
